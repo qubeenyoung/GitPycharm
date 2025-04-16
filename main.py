f593c973f8b9ca94fa3852564d9912e44a4c6d2d
qu-beenyoung <soyoung9323@naver.com>
2025-04-16 14:51:18 +0900

    Hello World 4

--- main.py
+++ main.py
@@ -16,4 +16,7 @@ if __name__ == '__main__':
     #adding new item in hotfix
     print_hi('Hello World 2')
 
+    # adding new item in hotfix
+    print_hi('Hello World 4')
+
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
